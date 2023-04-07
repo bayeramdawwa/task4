4b74ca33aa66560cff0bb251bfa3bf3a298f59f0
bayeramdawwa <beramdawa@gmail.com>
2023-04-07 14:40:00 +0300

    make it respo

---
+++ task4/assist/js/js.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementsByClassName("nav-menu2").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementsByClassName("nav-menu2").style.marginLeft = "0";
+}
